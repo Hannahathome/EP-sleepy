8eabdd06e9b4618ad3ad8b0a2c304a6458220bcf
Hannah van Iterson <hvi.athome@gmail.com>
2022-05-27 10:17:52 +0200

    Update Oocsi.ino

--- EP-Sleepy_arduino/Oocsi.ino
+++ EP-Sleepy_arduino/Oocsi.ino
@@ -1,38 +1,9 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-
-#//include <esp_now.h>
 #include <WiFi.h>
 #include <OOCSI.h>
 
 //OOCSI CONNECTION --> you need the name an password of your wifi network for this
-//const char* ssid = "your network name"; // SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
-//const char* password = "your network password"; // Password of your Wifi network.
-//
-//const char* OOCSIName = "your oocsi handle"; // name for connecting with OOCSI (unique handle)
-//const char* hostserver = "your oocsi address .nl"; // put the adress of your OOCSI server here, can be URL or IP address string
-
-//const char* ssid = "Langeboomgaard"; // SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
-//const char* password = "ACvI4152EK"; // Password of your Wifi network.
-////
-const char* ssid = "Van Dijk"; // SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
-const char* password = "NFuu6bbe4fuv"; // Password of your Wifi network.
-//
-//const char* ssid = "jandel0309"; // SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
-//const char* password = "jandel0309"; // Password of your Wifi network.
-
-const char* OOCSIName = "EP_SLEEPY_Hannah"; // name for connecting with OOCSI (unique handle)
-const char* hostserver = "oocsi.id.tue.nl"; // put the adress of your OOCSI server here, can be URL or IP address string
-
+const char* ssid = "your network name"; // SSID of your Wifi network, the library currently does not support WPA2 Enterprise networks
+const char* password = "your network password"; // Password of your Wifi network.
 
 
 
